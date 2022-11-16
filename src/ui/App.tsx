75fa5a53513552b5d8c04d41218484ee2b98a955
Aloxan12 <kvakvaaak@mail.ru>
2022-11-16 16:34:40 +0300

    refactor

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,15 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.scss';
 import {AppRouter} from "../AppRouter";
 import {AppHeader} from "./AppHeader";
-import {AppInput} from "./Components/AppInput/AppInput";
 
 function App() {
-    const [value, setValue] = useState('')
     return (
         <div className="App">
             <AppHeader />
-            <AppInput value={value} onChange={setValue}/>
             <AppRouter />
         </div>
     );
--- src/ui/Components/AppInput/AppInput.tsx
+++ src/ui/Components/AppInput/AppInput.tsx
@@ -1,11 +1,13 @@
 import React, {ChangeEvent} from 'react';
+import './AppInput.scss'
 
 interface IAppInputProps{
     value: string
     onChange: (value: string)=> void
+    border?: boolean
 }
 
-export const AppInput = ({value, onChange}:IAppInputProps) => {
+export const AppInput = ({value, onChange, border}:IAppInputProps) => {
 
     const onChangeHandler=(e: ChangeEvent<HTMLInputElement>)=>{
         const result = e.currentTarget.value
@@ -13,8 +15,8 @@ export const AppInput = ({value, onChange}:IAppInputProps) => {
     }
 
     return (
-        <div>
-            <input value={value} onChange={onChangeHandler}/>
+        <div className={'input-wrap'}>
+            <input className={`input-base ${border ? 'input-border' : ''}`} value={value} onChange={onChangeHandler}/>
         </div>
     )
 }
